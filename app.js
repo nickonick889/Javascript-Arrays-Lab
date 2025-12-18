d74d5be8e392cac7f83354ef80efe7f5954ffd1d
Nicholas Lai <nicholaslai889@gmail.com>
2025-12-18 19:27:59 +0800

    First commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Exercise 1 
+const foods = [];
+console.log('Exercise 1 results:', foods);
+
+// Exercise 2
+foods.push ('pizza' , 'cheeseburger')
+
+console.log('Exercise 2 result:', foods);
+
+// Exercise 3
+foods.unshift ('taco')
+
+console.log('Exercise 3 result:', foods);
+
+// Exercise 4
+const favFood = foods[1]
+
+console.log('Exercise 4 result:', favFood);
+
+// Exercise 5
+
+// Exercise 6
+
+// Exercise 7
+
+// Exercise 8
+
+// Exercise 9
